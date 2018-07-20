ddb4c5009d7f410def23908d593c04090b3c88ef
derekvandermark <derek.vandermark@gmail.com>
2018-07-20 13:16:10 -0400

    Initial Commit

---
+++ VHNavigationAppOfficial v 0.1Tests/VHNavigationAppOfficial_v_0_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  VHNavigationAppOfficial_v_0_1Tests.swift
+//  VHNavigationAppOfficial v 0.1Tests
+//
+//  Created by Derek Vandermark on 7/20/18.
+//  Copyright © 2018 Derek. All rights reserved.
+//
+
+import XCTest
+@testable import VHNavigationAppOfficial_v_0_1
+
+class VHNavigationAppOfficial_v_0_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
